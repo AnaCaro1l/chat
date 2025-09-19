267c9c13c70103459e2daceaff252a6cbc889928
David <david.hcandido1@gmail.com>
2025-09-19 14:38:57 -0300

    Adicionar limite para paginação

--- api/src/services/MessageServices/listMessagesService.ts
+++ api/src/services/MessageServices/listMessagesService.ts
@@ -3,26 +3,28 @@ import { Message } from '../../models/Message';
 interface Request {
   chatId: string;
   userId: number;
+  page: number;
+  pageSize: number;
 }
 export const listMessagesService = async ({
   chatId,
   userId,
+  page = 0,
+  pageSize = 20
 }: Request): Promise<Message[]> => {
+  const offset = page * pageSize;
   const messages = await Message.findAll({
     where: {
       chatId: chatId,
     },
+    order: [['createdAt', 'DESC']],
+    limit: pageSize,
+    offset,
   });
 
   for (const message of messages) {
     message.setDataValue('fromMe', message.fromUser === userId);
   }
-
-  messages.sort(
-    (a, b) =>
-      (new Date(a.createdAt).getTime() ?? 0) -
-      (new Date(b.createdAt).getTime() ?? 0)
-  );
 
   return messages;
 };
