497a6e9c685ddd35941a01702c5099df14a929d8
David <david.hcandido1@gmail.com>
2025-09-25 15:06:35 -0300

    feat(api): adicionar condicional de erro no sessionController

--- api/src/controllers/SessionController.ts
+++ api/src/controllers/SessionController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { User } from '../models/User';
 import { authUserService } from '../services/UserServices/authUserService';
+import { AppError } from '../errors/AppError';
 
 export const login = async (req: Request, res: Response): Promise<Response> => {
   const { email, password } = req.body;
@@ -8,6 +9,10 @@ export const login = async (req: Request, res: Response): Promise<Response> => {
   const user = await User.findOne({
     where: { email: email },
   });
+
+  if (!user) {
+    throw new AppError('User not found', 404)
+  }
 
   const token = await authUserService({ email, password });
 
